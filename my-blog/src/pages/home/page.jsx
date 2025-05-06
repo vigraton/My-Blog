a971fa77d392ab9538da82158c0e6aa55423d9ef
vigraton <viviangraton@gmail.com>
2025-05-06 10:39:56 -0300

    finish header

--- my-blog/src/pages/home/page.jsx
+++ my-blog/src/pages/home/page.jsx
@@ -6,8 +6,8 @@ import { Button } from "../../@/components/ui/button";
 function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="border-b">
-        <div className="container mx-auto py-4 px-4 sm:px-6 lg:px-8">
+      <header className="border-b ">
+        <div className="container mx-auto py-4 px-4 sm:px-6 lg:px-8 flex flex-row">
           <div className=" flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
             <Link href="/" className="flex items-center">
               <div
@@ -21,16 +21,16 @@ function Home() {
           </div>
 
           <div className="flex-1 max-w-md mx-auto md:mx-0 md:ml-8">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <div className="relative px-4">
+              <Search className="absolute left-6 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input
                 placeholder="Search posts..."
-                className="w-full pl-8 bg-background"
+                className="w-full pl-8 bg-background "
               />
             </div>
           </div>
 
-          <div className="flex items-center justify-center md:justify-end gap-4 mt-4 md:mt-0">
+          <div className="flex flex-row justify-center md:justify-end gap-4 mt-4 md:mt-0">
             <Button variant="outline" asChild>
               <Link href="/sign-in">Sing In</Link>
             </Button>
@@ -40,6 +40,8 @@ function Home() {
           </div>
         </div>
       </header>
+
+      <main></main>
     </div>
   );
 }
